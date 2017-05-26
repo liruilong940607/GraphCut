5d3ac5a5fd970d39a726477849575ffb807ec87b
李大龙 <dalong@ridais-MacBook-Pro.local>
2017-05-26 15:45:20 +0800

    Initial Commit

---
+++ Graphcut/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graphcut
+//
+//  Created by 李大龙 on 5/26/17.
+//  Copyright (c) 2017 李大龙. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
